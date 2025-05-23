aa52664dc67dd9f9c5590014801638e23f061388
GANESH NIKHIL <ganeshnikhil@Mac.lan>
2025-05-23 09:32:15 +0530

    datatypes info

---
+++ 02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";
+
+//alert("hello "); // we are using node js , not browser
+
+// code redability shoule be high
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// Prmitive data types 
+// number => 2 to power 53
+// bigint 
+// string = ""
+// boolean => true/flase
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+// Object
+
+console.log(typeof "nikhil");
+console.log((typeof null)); // object
+console.log(typeof undefined); // undefined 
+
+
+
+
