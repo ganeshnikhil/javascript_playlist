433d2a46d6318623a1f8cc6dc3905e860758fc40
GANESH NIKHIL <ganeshnikhil@Mac.lan>
2025-05-24 20:25:40 +0530

    summary of datatypes

---
+++ datatype-summary.js
@@ -0,0 +1,37 @@
+// primitive datatype 
+
+// 7 types : string , Number , Boolean , null , undefined , symbol , BigInt 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null
+let UserEmail; // undefined 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+const bigNumber = 34565437888890874n
+
+const heros = ["shaktiman","nagraj" ,"doga"];
+let myobj = {
+    name: "nikhil",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("hello world");
+}
+console.log(typeof heros);
+
+// null type of datatype is object 
+
+// Reference type (Non primitve)
+
+// Array , objects , Functions 
+
+
+
+
