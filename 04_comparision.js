09297657c0cf5b7fd226bacbf70f75e806dd2942
GANESH NIKHIL <ganeshnikhil@Mac.lan>
2025-05-24 19:50:36 +0530

    abstract comparisions

---
+++ 04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(1 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1); // check value only with dataytpe converison 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2); // check value with datatype
+
+
