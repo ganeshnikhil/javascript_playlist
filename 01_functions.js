49df1c9a38b9e3c21faa19d4fe427540a8626174
GANESH NIKHIL <ganeshnikhil@Mac.lan>
2025-07-25 18:00:28 +0530

    object and array in functions

--- 01_functions.js
+++ 01_functions.js
@@ -1,8 +1,8 @@
-console.log('H');
-console.log('E');
-console.log('L');
-console.log('L');
-console.log('o');
+// console.log('H');
+// console.log('E');
+// console.log('L');
+// console.log('L');
+// console.log('o');
 
 
 function sayhello(){
@@ -30,7 +30,7 @@ function addTwoNumbers(number1 , number2){
 
 const result = addTwoNumbers(3,5)
 
-console.log("Result: ",result);
+//console.log("Result: ",result);
 
 function loginUserMessage(username = "nikhil"){
     if (!username){
@@ -41,12 +41,38 @@ function loginUserMessage(username = "nikhil"){
 }
 
 // console.log(loginUserMessage("nikhil"))
-console.log(loginUserMessage("nikhil"))
+//console.log(loginUserMessage("nikhil"))
 
+// rest operator
+function calculateCartPrice(val1 , val2 , ...nums){
+    return nums
+}
 
+//console.log(calculateCartPrice(200 , 400 , 500));
 
+const user = {
+    username : "nikhil",
+    price:139
+}
 
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
 
+//handleObject(user)
+
+handleObject({
+    username:"Nikhil",
+    price: 599
+})
+
+const mynewArray = [200 , 400 , 100 , 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+//console.log(returnSecondValue(mynewArray));
+console.log(returnSecondValue([200 , 400 , 500 , 1000]));
 
 
 
