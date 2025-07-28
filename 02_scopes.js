9c301425b5aec9d69fbf17a79900ba09326d4aa9
GANESH NIKHIL <ganeshnikhil@Mac.lan>
2025-07-28 19:59:17 +0530

    more on scope

--- 02_scopes.js
+++ 02_scopes.js
@@ -6,13 +6,52 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("INNER : ", a);
+    //console.log("INNER : ", a);
 }
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);  it is available out of scope
 
+function one(){
+    const username = "nikhil"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+
+    //console.log(website);
+    two()
+}
 
 
+//one()
+
+if(true){
+    const username = "nikhil"
+    if(username === "nikhil"){
+        const website = " youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+
+//console.log(username);
+
+
+// ++++++++++++++++++++++++++++ interesting ++++++++++++++++++
+
+// basic function acess before intilization
+addone(5);
+function addone(num){
+    return num + 1
+}
+addone(5)
+
+// expressions cant acess before intilization
+//addTwo(5)
+const addTwo = function(num){
+    return num+2
+}
 
