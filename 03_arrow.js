7b88138894f178735c64cc3fb4f6314fae562c08
GANESH NIKHIL <ganeshnikhil@Mac.lan>
2025-07-29 14:18:13 +0530

    arrow function

---
+++ 03_arrow.js
@@ -0,0 +1,51 @@
+// this is for current context reference 
+const user = {
+    username: "nikhil",
+    price: 999,
+    welcomeMessage : function(){
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+        
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+//console.log(this);
+
+// function chai(){
+//     let username = "nikhil"
+//     console.log(this.username);
+// }
+
+//chai()
+
+// const chai = function (){
+//     let username = "nikhil"
+//     console.log(this);
+// }
+
+
+// arrow function 
+const chai = () => {
+    let username = "nikhil";
+    console.log(this);
+}
+//chai()
+
+// const addTwo = (num1 , num2) => {
+//     return num1 + num2
+// }
+
+// implicit return 
+//const addTwo = (num1 , num2) => (num1 + num2)
+
+const addTwo = (num1 , num2) => ({name: "nikhil"})
+console.log(addTwo(3,4))
+
+
+const myArray = [2,5,6,8,9]
+
